c6082fab883758e205d406ad83b4b698d6362dab
Jon Schlinkert <dev@sellside.com>
2013-11-30 05:57:35 -0500

    adds `ocd` option

--- tasks/prettify.js
+++ tasks/prettify.js
@@ -32,7 +32,8 @@ module.exports = function(grunt) {
       brace_style: "expand",
       preserve_newline: false,
       max_preserve_newline: 0,
-      wrap_line_length: 0
+      wrap_line_length: 0,
+      unformatted: ['code', 'pre', 'em', 'strong', 'span']
     });
 
     options.indent_size = options.indent;
@@ -59,6 +60,11 @@ module.exports = function(grunt) {
 
       // Reduce multiple newlines to a single newline
       output = (options.condense === true) ? condense(output) : output;
+
+      // Use at your own risk. This option will slow down the build.
+      // What does "ocd" mean? Just look at the function, then lookup
+      // ocd on wikipedia.
+      output = (options.ocd === true) ? ocd(output) : output;
 
       // Add a single newline above code comments.
       if(options.padcomments === true) {
@@ -89,13 +95,40 @@ module.exports = function(grunt) {
     return content;
   };
 
+  // Normalize and condense all newlines
   var condense = function(str) {
-    return str.replace(/(\n|\r){2,}/g, '\n');
+    return str.replace(/(\r\n|\n\r|\n|\r){2,}/g, '\n')
   };
 
+  // fix multiline, Bootstrap-style comments
   var padcomments = function(str, num) {
     var nl = _str.repeat('\n', (num || 1));
-    return str.replace(/(\s*<!--\s)/g, nl + '$1');
+    return str.replace(/(\s*)(<!--.+)\s*(===.+)?/g, nl + '$1$2$1$3')
+  };
+
+  var ocd = function(str) {
+    str = str
+      // Remove any empty lines at the top of a file.
+      .replace(/^\s*/g, '')
+      // make <li><a></li> on one line, but only when li > a
+      .replace(/(<li>)(\s*)(<a .+)(\s*)(<\/li>)/g, '$1 $3 $5')
+      // make <a><span></a> on one line, but only when a > span
+      .replace(/(<a.+)(\s*)(<span.+)(\s*)(<\/a>)/g, '$1 $3 $5')
+      // Put labels and inputs on one line
+      .replace(/(\s*)(<label>)(\s*)(.+)(\s*)(.+)\s*(.+)\s*(<\/label>)/g, '$1$2$4$6$7$8')
+      // Make <p>text</p> on one line.
+      .replace(/(<p.+)(\s*)(<\/p>)/g, '$1$3')
+      // Fix newlines when <p> has child elements
+      .replace(/(\s*)(<p.+)(\s*)(<.+)(\s*)(.+)(<\/p>)/g, '$1$2$3$4$5$6$1$7')
+      // Adjust spacing for span > input
+      .replace(/(\s*)(<(?:span|strong|button).+)(\s*)(<.+)(\s*)(<\/(?:span|strong|button)>)/g, '$1$2$1  $4$1$6')
+      // Add a newline for tags nested inside <h1-6>
+      .replace(/(\s*)(<h[0-6](?:.+)?>)(.*)(<(?:small|span|strong|em)(?:.+)?)(\s*)(<\/h[0-6]>)/g, '$1$2$3$1  $4$1$6')
+      // Fix conditional comments
+      .replace(/( *)(<!--\[.+)(\s*)(.+\s*)?(.+\s*)?(<!\[endif\]-->)/g, '$1$2\n  $1$4$1$5$1$6')
+      // Bring closing comments up to the same line as closing tag.
+      .replace(/\s*(<!--\s*\/.+)/g, '$1');
+    return str;
   };
 
   var prettifyHTML = function(source, options) {
